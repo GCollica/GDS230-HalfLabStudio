22fc580968744e5675e16e0a93dd33a4708b3b73
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-02-19 22:22:20 +0800

    changes to Sam Scene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int CashMoney;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CashMoney = 400;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(CashMoney);
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    //public GameObject bullet;
+    
+    //the transform of the enemy within range
+    public Transform enemy;
+
+
+    public Transform firePoint;
+    
+    //the speed that the turret turns towards the enemy in range
+    public float turnSpeed = 10f;
+
+
+    public float[] fireGunFloat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            if (!enemy)
+            {
+                enemy = collision.gameObject.transform;
+            }
+            
+           // Fire();
+            Turn();
+            
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            enemy = null;
+        }
+    }
+
+    void Turn()
+    {
+        Vector2 direction = enemy.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+    }
+
+   /* void Fire()
+    {
+        fireGunFloat[0] -= Time.deltaTime;
+        if (fireGunFloat[0] < 0f)
+        {
+            Instantiate(bullet, firePoint.position, transform.rotation);
+            fireGunFloat[0] = 5f;
+
+        }
+
+
+    }*/
+
+}
