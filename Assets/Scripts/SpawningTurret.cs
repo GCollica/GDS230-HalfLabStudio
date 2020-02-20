50a187b8a2d5932adf3a3062188a1e4a690102af
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-02-20 13:34:47 +0800

    prefab changes

--- Assets/Scripts/SpawningTurret.cs
+++ Assets/Scripts/SpawningTurret.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class SpawningTurret : MonoBehaviour
 {
+    //get this to follow mouse point, then click when in certain area and if placed in area charge money
+    private Vector3 mousePos;
+    Rigidbody2D rb;
+    Vector2 direction;
+    float moveSpeed = 1000f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
+    void FixedUpdate()
+    {
+        mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        direction = (mousePos - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+    }
     // Update is called once per frame
     void Update()
     {
