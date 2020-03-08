c43ac02d7d758ec02974786308aa6789c478b583
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-03-09 07:16:11 +0800

    Revert "update"
    This reverts commit 689fc0da3417a97370a0e52f2e4247374538e2a5.

--- Assets/Scripts/Riley's Scripts/BasicWaveSpawner.cs
+++ Assets/Scripts/Riley's Scripts/BasicWaveSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class BasicWaveSpawner : MonoBehaviour
 {
+    public static int EnemiesAlive = 0;
+
     public Transform leftEnemyPrefab;
     public Transform rightEnemyPrefab;
 
@@ -48,7 +50,10 @@ public class BasicWaveSpawner : MonoBehaviour
     void SpawnEnemy()
     {
         Instantiate(leftEnemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        EnemiesAlive++;
+
         Instantiate(rightEnemyPrefab, spawnPoint.position + rightEnemyOffset, spawnPoint.rotation);
+        EnemiesAlive++;
     }
 
 }
