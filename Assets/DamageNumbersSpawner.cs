c13f61379287758b851b9a5fce69e17d7cd3a23f
GCollica <49055809+GCollica@users.noreply.github.com>
2020-04-08 01:01:02 +0800

    Added Spawn Locations for DamageUI

--- Assets/DamageNumbersSpawner.cs
+++ Assets/DamageNumbersSpawner.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class DamageNumbersSpawner : MonoBehaviour
 {
     public GameObject textAsset;
+    public GameObject[] spawnPoints;
 
     public void SpawnDamageNumber(int damage)
     {
-        GameObject spawnedTextAsset = Instantiate(textAsset, this.gameObject.transform);
+        GameObject spawnedTextAsset = Instantiate(textAsset, ChoseSpawnPoint().transform);
         DamageNumbersUI damageNumbersUIScript = spawnedTextAsset.GetComponent<DamageNumbersUI>();
         damageNumbersUIScript.SetNumber(damage);
+    }
+
+    private GameObject ChoseSpawnPoint()
+    {
+        GameObject chosenSpawnPoint;
+
+        int randomInt = Mathf.RoundToInt(Random.Range(0f, spawnPoints.Length));
+        chosenSpawnPoint = spawnPoints[(randomInt - 1)];
+
+        return chosenSpawnPoint;
     }
 }
