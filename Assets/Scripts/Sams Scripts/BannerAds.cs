0869d65b404f722cc1f434fd9ccae4692fe86935
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-04-21 14:03:01 +0800

    s

--- Assets/Scripts/Sams Scripts/BannerAds.cs
+++ Assets/Scripts/Sams Scripts/BannerAds.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 
 public class BannerAds : MonoBehaviour
 {
-    public GameObject banner;
+    public GameObject[] banner;
     private bool stop;
-    private float startBanner = 2.5f;
-
+    private float startBanner = 0.5f;
     private void Update()
     {
-
         if (stop == false)
         {
             startBanner -= Time.deltaTime;
         }
         if (startBanner <= 0f) 
         {
-            banner.SetActive(true);
+            banner[Random.Range(0, 6)].SetActive(true);
             stop = true;
             startBanner = 5f;
         }
