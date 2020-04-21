50f039bb97f124726c1f8d0ce054e827ef89ece2
DaraghW <1015125@student.sae.edu.au>
2020-04-21 18:15:26 +0800

    Set up audio

---
+++ Assets/Audio/MixerCont.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerCont : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("sfx", volume);
+    }
+}
